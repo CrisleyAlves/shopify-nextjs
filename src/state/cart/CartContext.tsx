57b32776beb71c561f6efea267b02e98fb875c54
Crisley Alves <crisleyalvesphx@gmail.com>
2025-03-08 13:35:23 -0300

    add cart service

---
+++ src/state/cart/CartContext.tsx
@@ -0,0 +1,46 @@
+"use client";
+import {
+  createContext,
+  ReactElement,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+import { Cart } from "@/lib/shopify/cart/types";
+
+type CartContextType = {
+  cart: Cart | undefined;
+  updateShopifyCart: (payload: any) => void;
+  isEmpty: boolean;
+};
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export function CartProvider({
+  children,
+  shopifyCart,
+}: {
+  children: React.ReactNode;
+  shopifyCart: Cart | undefined;
+}): ReactElement<any, any> {
+  const [cart, setCart] = useState<Cart | undefined>(shopifyCart);
+
+  const updateShopifyCart = (payload: Cart) => setCart(payload);
+
+  const value = useMemo(
+    () => ({
+      cart,
+      updateShopifyCart,
+      isEmpty: !!cart?.lines?.length,
+    }),
+    [cart]
+  );
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) throw new Error("useCart must be used within a CartProvider");
+  return context;
+}
---
+++ src/state/cart/actions.ts
@@ -0,0 +1,104 @@
+"use server";
+import { revalidateTag } from "next/cache";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { TAGS } from "@/lib/shopify/constants";
+import {
+  addToCart,
+  createCart,
+  getCart,
+  removeFromCart,
+  updateCart,
+} from "@/lib/shopify/api/cart";
+
+export async function getCartIdFromCookies(): Promise<string | undefined> {
+  const cookie = await cookies();
+  const cartId = cookie.get("cartId")?.value;
+  return cartId;
+}
+
+export async function addItemToCart(selectedVariantId: string) {
+  const cookie = await cookies();
+  const cartId = cookie.get("cartId")?.value;
+
+  if (!cartId || !selectedVariantId) {
+    return new Error("Error adding item to cart");
+  }
+
+  try {
+    const response = await addToCart(cartId, [
+      { merchandiseId: selectedVariantId, quantity: 1 },
+    ]);
+    revalidateTag(TAGS.cart);
+    return response;
+  } catch (error) {
+    return new Error("Error adding item to cart");
+  }
+}
+
+export async function updateItemQuantity(payload: {
+  merchandiseId: string;
+  quantity: number;
+}) {
+  const cartId = await getCartIdFromCookies();
+  if (!cartId) {
+    return new Error("Missing cart ID");
+  }
+
+  const { merchandiseId, quantity } = payload;
+
+  try {
+    const cart = await getCart(cartId);
+    if (!cart) {
+      return new Error("Missing cart ID");
+    }
+
+    const lineItem = cart.lines.find(
+      (line) => line.merchandise.id === merchandiseId
+    );
+
+    const isExistentItem = lineItem && lineItem.id;
+    const shouldRemoveFromCart = isExistentItem && quantity === 0;
+
+    if (shouldRemoveFromCart)
+      return await removeFromCart(cartId, [lineItem.id]);
+    if (isExistentItem)
+      return await updateCart(cartId, [
+        {
+          id: lineItem.id,
+          merchandiseId,
+          quantity,
+        },
+      ]);
+
+    const shopifyCart = await addToCart(cartId, [{ merchandiseId, quantity }]);
+    revalidateTag(TAGS.cart);
+    return shopifyCart;
+  } catch (error) {
+    console.error(error);
+    return "Error updating item quantity";
+  }
+}
+
+export async function redirectToCheckout() {
+  const cartId = await getCartIdFromCookies();
+
+  if (!cartId) {
+    return "Missing cart ID";
+  }
+
+  const cart = await getCart(cartId);
+
+  if (!cart) {
+    return "Error fetching cart";
+  }
+
+  redirect(cart.checkoutUrl);
+}
+
+export async function createCartAndSetCookie() {
+  const cart = await createCart();
+  const cookie = await cookies();
+  cookie.set("cartId", cart.id!);
+}
