e314a72d84e6c5d04ecea143c7838deac50a9989
Crisley Alves <crisleyalvesphx@gmail.com>
2025-03-16 09:55:36 -0300

    create AppProviders

---
+++ src/context/AppProviders.tsx
@@ -0,0 +1,18 @@
+import { Cart } from "@/lib/shopify/cart/types";
+
+import { CartProvider } from "./CartContext";
+import { UIProvider } from "./UIContext";
+
+export function AppProviders({
+  children,
+  shopifyCart,
+}: {
+  children: React.ReactNode;
+  shopifyCart: Cart | undefined;
+}) {
+  return (
+    <UIProvider>
+      <CartProvider shopifyCart={shopifyCart}>{children}</CartProvider>
+    </UIProvider>
+  );
+}
