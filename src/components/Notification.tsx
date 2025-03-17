a878e980282a73bed4261ed10bf290e010c0c040
Crisley Alves <crisleyalvesphx@gmail.com>
2025-03-17 13:32:44 -0300

    Centralize notification to make it consitent

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -6,20 +6,16 @@ export default function Notification() {
   const { notification } = useUI();
 
   const bgColor = {
-    info: "bg-blue-100 text-blue-800 border-blue-300",
-    success: "bg-green-100 text-green-800 border-green-300",
-    warning: "bg-yellow-100 text-yellow-800 border-yellow-300",
-    error: "bg-red-100 text-red-800 border-red-300",
+    info: "bg-blue-300 text-blue-900 border-blue-600",
+    success: "bg-green-300 text-green-900 border-green-600",
+    warning: "bg-yellow-300 text-yellow-900 border-yellow-600",
+    error: "bg-red-300 text-red-900 border-red-600",
   }[notification.type];
 
   if (!notification.visible) return "";
 
   return (
-    <div
-      className="
-      fixed flex items-center w-full z-40 justify-center mt-3
-      md:justify-end md:pr-2"
-    >
+    <div className="fixed flex items-center w-full z-40 justify-center mt-20">
       <div
         className={`py-2 px-10 border-l-4 rounded-md ${bgColor} shadow-md max-w-sm font-light
         transition-all duration-500 transform ${
