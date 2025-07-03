6dd6ad8872a0a8f743b281b2f8b4a8a57e9b9bdc
Crisley Alves <crisleyalvesphx@gmail.com>
2025-07-03 11:52:40 -0300

    small fix for checkout page

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -1,3 +1,3 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="container p-5">{children}</div>;
+  return <div className="container p-5 min-h-[100vh]">{children}</div>;
 }
